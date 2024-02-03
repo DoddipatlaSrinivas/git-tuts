dbb852dc860d1d64b443f86ed065f04739108f79
srinivasdoddipatla <srinivasbobby143@gmail.com>
2024-02-04 00:01:26 +0530

    added dotenv express nodemon

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-console.log("Hello...");
-console.log("Hai srinivas.......");
-console.log("hai devOne.....");
+// Import the Express module
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Access the port number from environment variables or use a default value
+const PORT = process.env.PORT || 5000; // Set default port if PORT is not defined
+
+// Create an instance of the Express application
+const app = express();
+
+// Define a route for the homepage
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Set up the server to listen on the specified port
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('server started...!!!')
+});
+
+// Handle closing the server gracefully
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('Server stopped...!!!');
+        console.log(process.pid);
+        process.exit(0);
+    });
+});
